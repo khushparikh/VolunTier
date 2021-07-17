3018305a0e768458dbbe12b162d86fe38907907e
Yankeesfan123 <84161287+Yankeesfan123@users.noreply.github.com>
2021-07-16 21:18:50 -0400

    Add files via upload

---
+++ signup.js
@@ -0,0 +1,58 @@
+var computerScience;
+var compSciString;
+var athletics;
+var athleticsString;
+var engineering;
+var engineeringString;
+var therapy;
+var therapyString;
+var culinary;
+var culinaryString;
+var medicine;
+var medicineString;
+
+
+
+document.getElementById('CompSciButton').onclick = function() {
+   computerScience = true;
+}​;​
+
+document.getElementById('athleticsButton').onclick = function() {
+   athletics = true;
+}​;​
+
+document.getElementById('engineeringButton').onclick = function() {
+   engineering = true;
+}​;​
+
+document.getElementById('therapyButton').onclick = function() {
+   therapy = true;
+}​;​
+
+document.getElementById('medicineButton').onclick = function() {
+   medicine = true;
+}​;​
+
+if(computerScience = true) {
+   computerScienceString = "Computer Science"
+}
+
+if(athletics = true) {
+   athleticsString = "Athletics"
+}
+
+if(engineering = true) {
+   engineeringString = "Engineering"
+}
+
+if(therapy = true) {
+   therapyString = "Therapy"
+}
+
+if(culinary = true) {
+   culinaryString = "Culinary"
+}
+
+if(medicine = true) {
+   medicine = "Medicine"
+}
