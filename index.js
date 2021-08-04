73ed2734fa929ac64711f1c9ac8c013b8371873c
neelsk10 <84944588+neelsk10@users.noreply.github.com>
2021-08-04 12:01:19 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -7,9 +7,10 @@ const session = require('express-session');
 
 const mongoose = require('mongoose');
 
-const Profile = require('./models/profiles');
+const orgProfile = require('./models/orgprofiles');
+const Profile = require('./models/profiles')
 const Position = require('./models/positions')
-const User = require('./models/user')
+// const User = require('./models/user')
 
 const requireLogin = (req, res, next) => {
     if(!req.session.user_id){
@@ -40,46 +41,142 @@ app.use(methodOverride('_method'));
 app.use(session({secret: 'notagoodsecret'}));
 
 const categories1 = ['In-Person', 'Virtual'];
-const categories2 = ['Culinary', 'Engineering', 'Computer Science', 'Law', 'Business', 'Literature', 'Music', 'Finance', 'Cosmetics'];
+const categories2 = ['Culinary', 'Engineering', 'Computers', 'Law', 'Business', 'Literature', 'Music', 'Finance', 'Cosmetics'];
 
-app.get('/', (req, res) => {
-    res.send('THIS IS THE HOMEPAGE!')
+app.get('/home', async(req, res) => {
+    if(!req.session.user_id){
+        res.render('home.ejs')
+    }
+    else{
+        const user = await orgProfile.findById(req.session.user_id);
+        if(user){
+            res.render('orgHome.ejs', {user})
+        }
+        else{
+            const user = await Profile.findById(req.session.user_id);
+            if(user){
+                res.render('loggedInHome.ejs', {user})
+            }
+            else{
+                res.redirect('/login')
+            }
+        }
+    }
 })
 
-app.get('/register', (req, res) => {
-    res.render('register')
+app.get('/yourPositions', requireLogin, async(req, res) => {
+    const yourPositions = [];
+    const id = req.session.user_id;
+    const positions = await Position.find({})
+    for(let position of positions) { 
+        if(position.user_id == req.session.user_id){
+            yourPositions.push(position);
+        }
+    }
+    res.render('yourPositions.ejs', {yourPositions, id})
+}) 
+
+app.get('/yourPosition/:id', requireLogin, async(req, res) => {
+    const {id} = req.params;
+    const foundPosition = await Position.findById(id);
+    const foundUser = await orgProfile.findById(req.session.user_id);
+    if(foundPosition.user_id == req.session.user_id){
+        res.render('showYourPosition.ejs', {foundPosition, foundUser})
+    }
+    else{
+        res.redirect('/home');
+    }
+})  
+
+app.get('/orgRegister', async(req, res) => {
+    if(!req.session.user_id){
+        res.render('orgRegister', {categories2})
+    }
+    else{
+        res.redirect('/home')
+    }
 })
 
-app.post('/register', async (req, res) => {
-    const {password, username} = req.body;
+app.get('/register', async(req, res) => {
+    if(!req.session.user_id){
+        res.render('register', {categories2})
+    }
+    else{
+        res.redirect('/home')
+    }
+})
+
+app.post('/orgRegister', async (req, res) => {
+    const {password, username, orgName, townLocation, zipCode, taxID, interestTag1, phoneNum} = req.body;
     const hash = await bcrypt.hash(password, 12);
-    const notValidUser = await User.findOne({username});
+    const notValidUser = await orgProfile.findOne({username});
+    console.log(orgProfile);
+    console.log(username);
     if(notValidUser){
         res.send('Username is already taken')
     }
     else{
-        const user = new User({
+        const {Culinary, Engineering, Computers, Law, Business, Literature, Music, Finance, Cosmetics} = req.body;
+        const myInterests = [];
+        const interests = [Culinary, Engineering, Computers, Law, Business, Literature, Music, Finance, Cosmetics];
+        for(let interest of interests) { 
+            if(interest){
+                myInterests.push(interest);
+            }
+        }
+        console.log(myInterests);
+        const organization = new orgProfile({
             username, 
-            password: hash
+            password: hash,
+            orgName,
+            townLocation,
+            zipCode,
+            taxID,
+            interests: myInterests,
+            phoneNum
         })
-        await user.save();
-        console.log(user);
-        req.session.user_id = user._id;
-        res.redirect('/')
+        await organization.save();
+        console.log(organization);
+        req.session.user_id = orgProfile._id;
+        res.redirect('/orgProfilePage')
     }
 })
 
-app.get('/secret', requireLogin, (req, res) => {
-    res.render('secret.ejs')
-})
+// app.get('/secret', requireLogin, (req, res) => {
+//     res.render('secret.ejs')
+// })
+
+app.get('/orgProfilePage', requireLogin, async(req, res) => {
+    const foundUser = await orgProfile.findById(req.session.user_id);
+    res.render('orgProfilePage.ejs', {foundUser, categories1, categories2})
+})  
+
+app.get('/profilePage', requireLogin, async(req, res) => {
+    const foundUser = await Profile.findById(req.session.user_id);
+    res.render('profilePage.ejs', {foundUser, categories1, categories2})
+})  
 
 app.get('/login', (req, res) => {
-    res.render('login')
+    if(!req.session.user_id){
+        res.render('login')
+    }
+    else{
+        res.redirect('/home')
+    }
+})
+
+app.get('/orgLogin', (req, res) => {
+    if(!req.session.user_id){
+        res.render('orgLogin')
+    }
+    else{
+        res.redirect('/home')
+    }
 })
 
 app.post('/login', async (req, res) => {
-    const {username, password} = req.body;
-    const user = await User.findOne({username});
+    const {username, password, id} = req.body;
+    const user = await Profile.findOne({username});
     if(!user){
         res.redirect('/login')
     }
@@ -88,7 +185,7 @@ app.post('/login', async (req, res) => {
         const validPassword = await bcrypt.compare(password, user.password);
         if(validPassword){
             req.session.user_id = user._id;
-            res.redirect('/secret');
+            res.redirect('/profilePage');
         }
         else{
             res.redirect('/login');
@@ -96,13 +193,124 @@ app.post('/login', async (req, res) => {
     }
 })
 
+app.post('/orgLogin', async (req, res) => {
+    const {username, password, id} = req.body;
+    const user = await orgProfile.findOne({username});
+    if(!user){
+        res.send('wrong username')
+    }
+    else{
+        console.log(user.password);
+        const validPassword = await bcrypt.compare(password, user.password);
+        if(validPassword){
+            req.session.user_id = user._id;
+            res.redirect('/orgProfilePage');
+        }
+        else{
+            res.send('wrong password');
+        }
+    }
+})
+
+
 app.post('/logout', (req, res) => {
     req.session.destroy();
-    res.redirect('/');
+    res.redirect('/home');
 })
 
 app.get('/profiles', async (req, res) => { 
-    const profiles = await Profile.find({})
+    const users = await orgProfile.find({})
+    res.render('index.ejs', {users})
+})
+
+
+app.post('/register', async (req, res) => {
+    const {password, username, name, location, zipCode} = req.body;
+    const hash = await bcrypt.hash(password, 12);
+    const notValidUser = await Profile.findOne({username});
+    console.log(username);
+    if(notValidUser){
+        res.send('Username is already taken')
+    }
+    else{
+        const {Culinary, Engineering, Computers, Law, Business, Literature, Music, Finance, Cosmetics} = req.body;
+        const myInterests = [];
+        const interests = [Culinary, Engineering, Computers, Law, Business, Literature, Music, Finance, Cosmetics];
+        for(let interest of interests) { 
+            if(interest){
+                myInterests.push(interest);
+            }
+        }
+        console.log(myInterests);
+        const Student = new Profile({
+            username, 
+            password: hash,
+            name,
+            location,
+            zipCode,
+            interests: myInterests
+        })
+        await Student.save();
+        console.log(Student);
+        req.session.user_id = Student._id;
+        res.redirect('/profilePage')
+    }
+})
+
+// app.post('/profiles', async (req, res) => {
+//     const newProfile = new Profile(req.body);
+//     await newProfile.save();
+//     console.log(newProfile);
+//     res.redirect(`/profiles/${newProfile.id}`)
+// })
+
+app.get('/profiles/:id', async(req, res) => {
+    const {id} = req.params;
+    const foundUser = await orgProfile.findById(id);
+    console.log(foundUser);
+    res.render('show.ejs', {foundUser});
+})  
+
+app.get('/edit', requireLogin, async(req, res) => {
+    const foundUser = await Profile.findById(req.session.user_id);
+    res.render('edit.ejs', {foundUser, categories1, categories2});
+})  
+
+app.get('/orgEdit', requireLogin, async(req, res) => {
+    const foundUser = await orgProfile.findById(req.session.user_id);
+    res.render('orgEdit.ejs', {foundUser, categories1, categories2});
+})  
+
+app.put('/profiles', async(req, res) => {
+    const editProfile = await Profile.findByIdAndUpdate(req.session.user_id, req.body, {runValidators: true, new: true});
+    console.log(req.body);
+    res.redirect('/profilePage');
+})
+
+app.put('/orgProfiles', async(req, res) => {
+    const editProfile = await orgProfile.findByIdAndUpdate(req.session.user_id, req.body, {runValidators: true, new: true});
+    console.log(req.body);
+    res.redirect('/orgProfilePage');
+})
+
+
+app.delete('/profilePage/:id', async(req, res) => {
+    const {id} = req.params;
+    const deletedProfile = await Profile.findByIdAndDelete(id);
+    req.session.destroy();
+    res.redirect('/home')
+})
+
+app.delete('/orgProfilePage/:id', async(req, res) => {
+    const {id} = req.params;
+    console.log(id);
+    const deletedProfile = await orgProfile.findByIdAndDelete(id);
+    req.session.destroy();
+    res.redirect('/home')
+})
+
+app.get('/orgProfiles', async (req, res) => { 
+    const profiles = await orgProfile.find({})
     res.render('index.ejs', {profiles})
 })
 
@@ -111,68 +319,100 @@ app.get('/positions', async (req, res) => {
     res.render('listPositions.ejs', {positions})
 })
 
-
-app.get('/profiles/:id/addPosition', async(req, res) => {
-    const {id} = req.params;
-    const foundProfile = await Profile.findById(id);
-    res.render('addPosition.ejs', {foundProfile, categories1, categories2});
+app.get('/addPosition', requireLogin, async(req, res) => {
+    const foundUser = await orgProfile.findById(req.session.user_id);
+    res.render('addPosition.ejs', {foundUser, categories1, categories2});
 })  
 
-app.get('/profiles/new', (req, res) => {
-    res.render('new.ejs', {categories1, categories2})
-}) 
+// app.get('/orgprofiles/new', (req, res) => {
+//     res.render('newOrg.ejs', {categories1, categories2})
+// }) 
 
-app.post('/profiles', async (req, res) => {
-    const newProfile = new Profile(req.body);
-    await newProfile.save();
-    console.log(newProfile);
-    res.redirect(`/profiles/${newProfile.id}`)
-})
+// app.post('/orgProfiles', async (req, res) => {
+//     const newProfile = new orgProfile(req.body);
+//     await newProfile.save();
+//     console.log(newProfile);
+//     res.redirect(`/orgprofiles/${newProfile.id}`)
+// })
 
 app.post('/positions', async (req, res) => {
-    const newPosition = new Position(req.body);
-    await newPosition.save();
-    console.log(newPosition);
-    res.redirect(`position/${newPosition.id}`)
+    const {positionName, username, positionLocation, positionZipCode, location, interestTag2, user_id} = req.body;
+    const user = await orgProfile.findOne({username});
+    if(!user){
+        res.redirect('/addPosition')
+    }
+    else{
+        if(user.id != req.session.user_id){
+            res.redirect('/addPosition')
+        }
+        else{
+            const {Culinary, Engineering, Computers, Law, Business, Literature, Music, Finance, Cosmetics} = req.body;
+            const myInterests = [];
+            const interests = [Culinary, Engineering, Computers, Law, Business, Literature, Music, Finance, Cosmetics];
+            for(let interest of interests) { 
+                if(interest){
+                    myInterests.push(interest);
+                } 
+            }
+            const newPosition = new Position({
+                positionName,
+                orgName: user.orgName, 
+                positionLocation,
+                positionZipCode,
+                location,
+                interests: myInterests,
+                user_id: req.session.user_id,
+            })
+            await newPosition.save();
+            console.log(newPosition);
+            res.redirect(`/position/${user.id}/${newPosition.id}`)
+        }
+    }
 })
 
-app.get('/profiles/:id', async(req, res) => {
+app.get('/orgProfiles/:id', async(req, res) => {
     const {id} = req.params;
-    const foundProfile = await Profile.findById(id);
+    const foundProfile = await orgProfile.findById(id);
     console.log(foundProfile);
-    res.render('show.ejs', {foundProfile})
+    res.render('orgShow.ejs', {foundProfile})
 })  
 
-app.get('/position/:id', async(req, res) => {
-    const {id} = req.params;
+app.get('/position/:user_id/:id', async(req, res) => {
+    const {user_id, id} = req.params;
     const foundPosition = await Position.findById(id);
+    const foundUser = await orgProfile.findById(user_id);
     console.log(foundPosition);
-    res.render('showPosition.ejs', {foundPosition})
-})  
+    console.log(foundUser);
+    res.render('showPosition.ejs', {foundPosition, foundUser})
+})    
 
-app.get('/profiles/:id/edit', async(req, res) => {
-    const {id} = req.params;
-    const foundProfile = await Profile.findById(id);
-    res.render('edit.ejs', {foundProfile, categories1, categories2});
-})  
 
-app.put('/profiles/:id', async(req, res) => {
-    const {id} = req.params;
-    const editProfile = await Profile.findByIdAndUpdate(id, req.body, {runValidators: true, new: true});
-    console.log(req.body);
-    res.redirect(`/profiles/${editProfile._id}`);
-})
+// app.get('/orgProfiles/:id/edit', async(req, res) => {
+//     const {id} = req.params;
+//     const foundProfile = await orgProfile.findById(id);
+//     res.render('editOrg.ejs', {foundProfile, categories1, categories2});
+// })  
 
-app.delete('/profiles/:id', async(req, res) => {
-    const {id} = req.params;
-    const deletedProfile = await Profile.findByIdAndDelete(id);
-    res.redirect('/profiles')
-})
+
+// app.delete('/orgprofiles/:id', async(req, res) => {
+//     const {id} = req.params;
+//     const deletedProfile = await orgProfile.findByIdAndDelete(id);
+//     res.redirect('/orgprofiles')
+// })
 
 app.delete('/positions/:id', async(req, res) => {
     const {id} = req.params;
-    const deletedPosition = await Position.findByIdAndDelete(id);
-    res.redirect('/positions')
+    const position = await Position.findById(id);
+    console.log(position.user_id);
+    console.log(req.session.user_id);
+    if(position.user_id == req.session.user_id){
+        await Position.findById(id);
+        const deletedProfile = await Position.findByIdAndDelete(id);
+        res.redirect('/yourPositions')
+    }
+    else{
+        res.redirect('/home')
+    }
 })
 
 app.listen(3000, () =>{
